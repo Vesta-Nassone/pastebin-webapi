f3fa96e893715e862c4423f30695e288aaf0ddc6
Vesta-Nassone <vestanassone@gmail.com>
2022-01-06 16:01:33 +0200

    Define the create method for SnippetSerializer class.

--- serializers/snippets/serializers.py
+++ serializers/snippets/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+from .models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
 
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
     linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    language = serializers.ChoiceField(
+        choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return super().create(validated_data)
