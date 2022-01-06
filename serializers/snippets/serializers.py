19b816f5a75052a7b0edf8ce5c3f59a128e87623
Vesta-Nassone <vestanassone@gmail.com>
2022-01-06 16:06:53 +0200

    Override the update method for SnipperSerializer.

--- serializers/snippets/serializers.py
+++ serializers/snippets/serializers.py
@@ -22,4 +22,10 @@ class SnippetSerializer(serializers.Serializer):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
-        return super().update(instance, validated_data)
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
