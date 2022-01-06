78e88aedee3cd1bf5564d47bcb0d4a4986508eba
Vesta-Nassone <vestanassone@gmail.com>
2022-01-06 17:03:38 +0200

    Add the snippet detail url path.

--- serializers/serializers/urls.py
+++ serializers/serializers/urls.py
@@ -20,4 +20,5 @@ from snippets import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("snippets/", views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail)
 ]
