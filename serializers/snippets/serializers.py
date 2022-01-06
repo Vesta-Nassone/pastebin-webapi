0aa80b95bca7a5421f107932b32cf36298b012f7
Vesta-Nassone <vestanassone@gmail.com>
2022-01-06 16:00:20 +0200

    Implement the SnipperSerializer class.

--- serializers/snippets/serializers.py
+++ serializers/snippets/serializers.py
@@ -2,4 +2,10 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    
