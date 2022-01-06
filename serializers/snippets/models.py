eeaadbf503f27eba8c2dbf76f2e542ee1c8ba373
Vesta-Nassone <vestanassone@gmail.com>
2022-01-06 15:32:43 +0200

    Create the Snippets model class.

--- serializers/snippets/models.py
+++ serializers/snippets/models.py
@@ -7,7 +7,8 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICE = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
-
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     
 
 
