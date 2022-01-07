e86858efd1afc94b6a6fd85ce530c17f40488703
Vesta-Nassone <vestanassone@gmail.com>
2022-01-07 22:14:35 +0200

    Import the CBV from rest framework.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
 from django.http.response import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Snippet
 from .serializers import SnippetSerializer
