6873ddf8be8e6adcad2f4c0738b06bd4f20f85af
Vesta-Nassone <vestanassone@gmail.com>
2022-01-07 22:27:28 +0200

    Override the get method for API view.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -14,4 +14,6 @@ class SnippetList(APIView):
     List all snippets, or create a new snippet.
     """
     def get(self, request, format=None):
-        
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
