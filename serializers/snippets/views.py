dc2d28236a0107ac009f9d91ef1fbc947c27dc92
Vesta-Nassone <vestanassone@gmail.com>
2022-01-09 22:15:44 +0200

    Explicitly bind the methods to actions.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -22,3 +22,12 @@ class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
 class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
