31eb9a7960f341353901d45c93892e349ea72afd
Vesta-Nassone <vestanassone@gmail.com>
2022-01-09 21:29:48 +0200

    Define the Snippet detail class.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -26,3 +26,6 @@ class SnippetList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SnippetDetailView(APIView):
+    
