d20421e5befbaadfcc0d63efb0f0b5bd9ea8d11d
Vesta-Nassone <vestanassone@gmail.com>
2022-01-10 21:05:21 +0200

    Implement the UserDetail view.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -20,5 +20,7 @@ class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class UserDetail(generics.RetrieveAPIView):
-    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
