fbac921412c1a9f7e370075bf927628f35029a53
Vesta-Nassone <vestanassone@gmail.com>
2022-01-11 22:09:49 +0200

    Import the rest renderers.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
