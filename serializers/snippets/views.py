2f2b97b1e0564f62fe52f6da17c971fec93a3dd0
Vesta-Nassone <vestanassone@gmail.com>
2022-01-12 23:22:56 +0200

    Replace the SnippetDetail/List/Highlight with the SnippetViewSet.

--- serializers/snippets/views.py
+++ serializers/snippets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions, renderers, viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -8,7 +8,8 @@ from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
-
+class SnippetViewSet(viewsets.ModelViewSet):
+    
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `retrieve` actions.
