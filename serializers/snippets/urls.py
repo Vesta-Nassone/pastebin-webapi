5964b57c256a5b70f722b63fe168db015c8e25f4
Vesta-Nassone <vestanassone@gmail.com>
2022-01-13 23:00:07 +0200

    Import the renderers into snipper urls.

--- serializers/snippets/urls.py
+++ serializers/snippets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
+from rest_framework import renderers
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
